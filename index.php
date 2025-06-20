74de09845bc00ab1566bf0840d67593c2bbb6901
Yuz <wwowkdrr@gmail.com>
2025-06-20 11:26:26 +0800

    繼承的範例

--- index.php
+++ index.php
@@ -8,26 +8,64 @@
 <body>
 <?php
 class Person {
+    //成員,屬性
+    //public:公開的屬性，可以在任何地方訪問，可讀可寫
+    //private:私有的屬性，只能在類別(物件)內部訪問
+    //protected:受保護的屬性，只能在類別(物件)內部或子類中訪問
+
     public $name;
     public $age;
 
-    public function __construct($name , $age){
-        $this->name =$name;
+    //方法,行為,透過建構函式告知name跟age  
+    public function __construct($name , $age){//沒寫屬性的話預設都是public
+        $this->name =$name;//this = Person
         $this->age =$age;
     }
 
-    public function great(){
+    //方法
+    //屬性不能直接被提取，要透過方法
+    public function greet(){
         echo "Hello my name is {$this->name} and  I'm  {$this->age} years old. <br>";
+    }
+    function getName(){
+        return $this->name;
+    }
+    function getAge(){
+        return $this->age;
+    }
+    function setName($name){
+        return $this->name=$name;
+    }
+    function setAge($Age){
+        return $this->name=$Age;
     }
 }    
     
 
 $jason = new Person('jason',18);
-echo $jason->name;
+echo $jason->getName();
 echo "<br>" ;
-echo $jason->age;
+echo $jason->getAge();
 echo "<br>" ;
-$jason->great();
+$jason->greet();
+
+echo "<hr>";
+
+$jason->setName("Mary");
+//$jason->age=20;
+echo $jason->getName();
+echo "<br>";
+echo $jason->getAge();   
+echo "<br>";
+$jason->greet();
+
+?>
+
+<hr>
+<h2>繼承</h2>
+<?php
+
+
 
 ?>
 </body>
