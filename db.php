a80fea558daa5bf7a5751fcf12662499081077e0
Yuz <wwowkdrr@gmail.com>
2025-06-20 16:03:06 +0800

    將自訂函式刪除,只留下class db

--- db.php
+++ db.php
@@ -1,6 +1,38 @@
-<?php
-$dsn = "mysql:host=localhost;dbname=store;charset=utf8";
-$pdo = new PDO($dsn, 'root', '');
+<?php 
+
+/*
+dd($array)
+$array: 要輸出的陣列 (array)
+功能: 以pre格式印出陣列內容，方便除錯
+*/
+function dd($array){
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+
+
+/*
+q($sql)
+$sql: 欲執行的SQL語法 (string)
+回傳: 查詢結果的關聯式陣列
+*/
+function q($sql){
+    $dsn="mysql:host=localhost;dbname=store;charset=utf8";
+    $pdo=new PDO($dsn,'root','');
+
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    
+}
+
+class DB{
+    private $pdo;
+    private $dsn="mysql:host=localhost;dbname=store;charset=utf8";
+    private $table;
+    public function __construct($table){
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+    }
 
 /*
 all($table, $array = null, $str = null)
@@ -9,36 +41,23 @@ $array: 條件陣列 (array|null)，可選，若為陣列則產生 WHERE 條件
 $str: 其他SQL語法 (string|null)，可選，附加在SQL語句後
 回傳: 查詢結果的關聯式陣列
 */
-function all($table, $array = null, $str = null)
-{
-    global $pdo;
+function all($array=null,$str=null){
+   
+    $sql="SELECT * FROM $this->table ";
 
-    $sql = "SELECT * FROM $table ";
+        if(is_array($array)){
+            $tmp=$this->array2sql($array);
+            $sql = $sql ." WHERE ".join(" AND ", $tmp);
+        }else{
+            $sql .= $array;
+        }
 
-    if (is_array($array)) {
-        $tmp = array2sql($array);
-        $sql = $sql . " WHERE " . join(" AND ", $tmp);
-    } else {
-        $sql .= $array;
-    }
-
-    $sql .= $str;
-
+        $sql .= $str;
+ 
     // echo $sql;
-    $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    $rows=$this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     return $rows;
-}
 
-/*
-dd($array)
-$array: 要輸出的陣列 (array)
-功能: 以pre格式印出陣列內容，方便除錯
-*/
-function dd($array)
-{
-    echo "<pre>";
-    print_r($array);
-    echo "</pre>";
 }
 
 /*
@@ -49,20 +68,17 @@ $id: 主鍵值或條件陣列 (int|string|array)
     - 若為單一值，則以id欄位查詢
 回傳: 查詢到的單筆資料 (關聯式陣列)
 */
-function find($table, $id)
-{
-    global $pdo;
-
-    if (is_array($id)) {
-        $tmp = array2sql($id);
-        $sql = "SELECT * FROM $table WHERE " . join(" AND ", $tmp);
-    } else {
-        $sql = "SELECT * FROM $table WHERE id='$id'";
+function find($id){
+    if(is_array($id)){
+        $tmp=$this->array2sql($id);
+        $sql="SELECT * FROM $this->table WHERE ".join(" AND ",$tmp);
+    }else{
+        $sql="SELECT * FROM $this->table WHERE id='$id'";
     }
 
     //echo  $sql;
 
-    return $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
 }
 
 /*
@@ -71,16 +87,15 @@ $table: 資料表名稱 (string)
 $data: 欲更新的資料陣列，必須包含id欄位 (array)
 回傳: 執行結果 (成功筆數)
 */
-function update($table, $data)
-{
-    global $pdo;
-    $tmp = array2sql($data);
+function update($data){
+    $tmp=$this->array2sql($data);
 
-    $sql = "UPDATE $table SET " . join(" , ", $tmp) . "
+    $sql="UPDATE $this->table SET ".join(" , ",$tmp)."
                       WHERE id='{$data['id']}'";
+    
+     echo $sql;
+    return $this->pdo->exec($sql);
 
-    echo $sql;
-    return $pdo->exec($sql);
 }
 
 /*
@@ -89,14 +104,12 @@ $table: 資料表名稱 (string)
 $data: 欲新增的資料陣列 (array)
 回傳: 執行結果 (成功筆數)
 */
-function insert($table, $data)
-{
-    global $pdo;
-    $keys = array_keys($data);
+function insert($data){
 
-    $sql = "INSERT INTO $table (`" . join("`,`", $keys) . "`) values('" . join("','", $data) . "');";
+    $keys=array_keys($data);
+    $sql="INSERT INTO $this->table (`".join("`,`",$keys)."`) values('".join("','",$data)."');";
     echo $sql;
-    return $pdo->exec($sql);
+    return $this->pdo->exec($sql);
 }
 
 /*
@@ -105,12 +118,11 @@ $table: 資料表名稱 (string)
 $data: 欲儲存的資料陣列 (array)
 功能: 若$data有id欄位則執行update，否則執行insert
 */
-function save($table, $data)
-{
-    if (isset($data['id'])) {
-        update($table, $data);
-    } else {
-        insert($table, $data);
+function save($data){
+    if(isset($data['id'])){
+        $this->update($this->table,$data);
+    }else{
+        $this->insert($this->table,$data);
     }
 }
 
@@ -122,20 +134,18 @@ $id: 主鍵值或條件陣列 (int|string|array)
     - 若為單一值，則以id欄位刪除
 回傳: 執行結果 (成功筆數)
 */
-function del($table, $id)
-{
-    global $pdo;
-    $sql = "DELETE FROM $table WHERE ";
-    if (is_array($id)) {
-        $tmp = array2sql($id);
-        $sql .= join(" AND ", $tmp);
-    } else {
+function del($id){
+    $sql="DELETE FROM $this->table WHERE ";
+    if(is_array($id)){
+        $tmp=$this->array2sql($id);
+        $sql .= join(" AND ",$tmp);
+    }else{
         $sql .= "id='$id'";
     }
 
     //echo  $sql;
 
-    return $pdo->exec($sql);
+    return $this->pdo->exec($sql);
 }
 
 /*
@@ -143,67 +153,22 @@ array2sql($array)
 $array: 欲轉換的條件或資料陣列 (array)
 回傳: 轉換成SQL語法片段的陣列 (array)
 */
-function array2sql($array)
-{
-    $tmp = [];
-    foreach ($array as $key => $value) {
-        $tmp[] = "`$key`='$value'";
+private function array2sql($array){
+    $tmp=[];
+    foreach($array as $key=>$value){
+        $tmp[]="`$key`='$value'";
     }
 
     return $tmp;
 }
 
-/*
-q($sql)
-$sql: 欲執行的SQL語法 (string)
-回傳: 查詢結果的關聯式陣列
-*/
-function q($sql)
-{
-    global $pdo;
-
-    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+public function q($sql){
+    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
 }
 
+$Item=new DB('items');
+$Sales=new DB('sales');
+dd($Item->find(['id'=>4]));
 
-class DB
-{
-    private $pdo;
-    private $dsn = "mysql:host=localhost;dbname=store;charset=utf8";
-    private $table;
-    public function __construct($table)
-    {
-        $this->table = $table;
-        $this->pdo = new PDO($this->dsn, 'root', '');
-    }
-
-    /*
-    all($table, $array = null, $str = null)
-    $table: 資料表名稱 (string)
-    $array: 條件陣列 (array|null)，可選，若為陣列則產生 WHERE 條件
-    $str: 其他SQL語法 (string|null)，可選，附加在SQL語句後
-    回傳: 查詢結果的關聯式陣列
-    */
-
-    function all($array = null, $str = null)
-    {
-
-        $sql = "SELECT * FROM $this->table ";
-
-        if (is_array($array)) {
-            $tmp = array2sql($array);
-            $sql = $sql . " WHERE " . join(" AND ", $tmp);
-        } else {
-            $sql .= $array;
-        }
-
-        $sql .= $str;
-
-        // echo $sql;
-        $rows = $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-        return $rows;
-    }
-}
-
-$Item = new DB('items');
-dd($Item->all());
+$Item->q($this->$sql);
